65bbe22830e72e33706c9614fad6531c75363a21
CantWeAllDisagree <cantwe@tutanota.de>
2023-05-04 16:36:40 -0400

    I might be close? Idk how this shit works lmao

--- src/main/java/cantwe/alldisagree/CWADForge/mixin/client/FileResourcePackProviderMixin.java
+++ src/main/java/cantwe/alldisagree/CWADForge/mixin/client/FileResourcePackProviderMixin.java
@@ -1,22 +1,41 @@
 package cantwe.alldisagree.CWADForge.mixin.client;
 
 
+import cantwe.alldisagree.CWADForge.resource.SussyResourcePack;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.resource.FileResourcePackProvider;
+import net.minecraft.resource.ResourcePackProfile;
+import net.minecraft.resource.ResourcePackSource;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
+
 //POTENTIALLY USE //TODO this causes crashes + Is reason for missing textures
-/*@Environment(EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 @Mixin(FileResourcePackProvider.class)
 public abstract class FileResourcePackProviderMixin {
 
-    @Shadow @Final
+
+    @Final
     private ResourceType type;
 
     @Inject(method = "register", at = @At("HEAD"))
-    private void reg(Consumer<ResourcePackProfile> profileAdder, CallbackInfo ci) {
+    private void reg(Consumer<ResourcePackProfile> profileAdder, ResourcePackProfile.Factory factory, CallbackInfo ci) {
 
-        ResourcePackProfile profile = ResourcePackProfile.create("Sussy Resource Pack", Text.literal("Sussy Resource Pack"),
-                true, (name) -> new SussyResourcePack("Sussy Resource Pack"), this.type, ResourcePackProfile.InsertionPosition.BOTTOM,
-                ResourcePackSource.create((param) -> Text.literal("sussylib"), false));
+        ResourcePackProfile profile = ResourcePackProfile.of("Sussy Resource Pack", Text.literal("Sussy Resource Pack"),
+                true, () -> new SussyResourcePack("Sussy Resource Pack"), this.type, ResourcePackProfile.InsertionPosition.BOTTOM,
+                ResourcePackSource.onlyName());
 
         if (profile != null) {
             profileAdder.accept(profile);
         }
-    }*/
+    }
+}
